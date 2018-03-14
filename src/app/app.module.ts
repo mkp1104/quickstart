f2d152c4c4c713994c919687c03a7a0bc4d8c45f
Prasad <Manishkumar.Prasad@CenturyLink.com>
2018-03-14 19:42:19 +0530

    3/14/2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+// import { AppComponentManish }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
+
+// export class AppComponent { }
