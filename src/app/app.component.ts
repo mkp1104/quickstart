46f5412e727d3be58972296a1a2cfe1883498e87
mkp1104 <manish.aec1104@gmail.com>
2018-03-16 08:57:05 +0530

    16-03-2018/intellJ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,44 @@
+// import { Component } from '@angular/core';
+
 import { Component } from '@angular/core';
-
-@Component({
+import { IProduct } from './product';
+import { ProductService } from './products.service';
+// import { appService } from './app.service';
+import { Http , Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+// @Component({
+//   selector: 'my-app',
+//   templateUrl: 'app/app.component.html',
+// })
+@Component ({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html',
+  template: '<div>Hello</div>',
+  providers: [ProductService]
 })
-export class AppComponent  {
-  appTitle: string = 'Welcome';
-  appStatus: boolean = true;
-  appList: any[] = [ {
-    'ID': '1',
-    'Name' : 'One'
- },
-
- {
-    'ID': '2',
-    'Name' : 'Two'
- } ];
+export   class   AppComponent  {
+  iproducts: IProduct[];
+  constructor(private _product: ProductService) {
+  }
+  
+  ngOnInit() : void {
+     this._product.getproducts()
+     .subscribe(iproducts => this.iproducts = iproducts);
+  }
 }
+// export class AppComponent  {
+//   appTitle: string = 'Welcome';
+//   appStatus: boolean = true;
+//   appList: any[] = [ {
+//     'ID': '1',
+//     'Name' : 'One'
+//  },
+
+//  {
+//     'ID': '2',
+//     'Name' : 'Two'
+//  } ];
+// }
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent }  from './app.component';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { Appproduct } from './product.component';
+import { AppInventory } from './Inventory.component';
 // import { AppComponentManish }  from './app.component';
-
+const appRoutes: Routes = [
+  { path: 'Product', component: Appproduct },
+  { path: 'Inventory', component: AppInventory },
+];
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ],
+  imports: [ BrowserModule,
+    RouterModule.forRoot(appRoutes)],
+    declarations: [ AppComponent,Appproduct,AppInventory],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
 
